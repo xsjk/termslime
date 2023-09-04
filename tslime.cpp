f43c1920b45eb3ff278dfebf08d3bc51c5add1d9
xsjk <xsjk17@outlook.com>
2023-09-04 23:04:48 +0800

    initial framework

---
+++ tslime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+#include "tslime.hpp"
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+
+    
+}
